dc204d74a21f5f666aa0551b4716ae7dfee12aff
artembarkovskiy <artem.barkovskyi@oa.edu.ua>
2024-09-24 20:27:25 +0300

    lab3

--- lab3/src/App.jsx
+++ lab3/src/App.jsx
@@ -1,33 +1,12 @@
-import { useState } from 'react';
-import PageTitle from '../components/PageTitle';
-import TaskForm from '../components/TaskForm';
-import TaskList from '../components/TaskList';
-import TaskSearch from '../components/TaskSearch';
-import '../src/App.css';
+import PageTitle from './components/PageTitle';
+import ToDoContainer from './components/ToDoContainer';
+import './App.css';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const [searchTitle, setSearchTitle] = useState('');
-
-  function handleAddTask(task) {
-    setTasks((prev) => [task, ...prev]);
-  }
-  
-
-  function handleDeleteTask(id) {
-    setTasks((prev) => prev.filter((x) => x.id !== id));
-  }
-
-  const filteredTasks = tasks.filter((x) =>
-    x.title.toLowerCase().includes(searchTitle.toLowerCase())
-  );
-
   return (
     <>
       <PageTitle />
-      <TaskSearch searchTitle={searchTitle} setSearchTitle={setSearchTitle} />
-      <TaskForm onAddTask={handleAddTask} />
-      <TaskList tasks={filteredTasks} onDeleteTask={handleDeleteTask} />
+      <ToDoContainer />
     </>
   );
 }
