a23e0103f20227d41a657fbde2b79c20a345bbab
artembarkovskiy <artem.barkovskyi@oa.edu.ua>
2024-10-08 10:22:47 +0300

    lab8

---
+++ lab8/src/App.jsx
@@ -0,0 +1,15 @@
+import ToDoContainer from "./components/ToDoContainer";
+import PageTitle from "./components/PageTitle";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <PageTitle />
+      <ToDoContainer />
+    </>
+  );
+}
+
+export default App;
---
+++ lab8/src/components/TaskSearch.jsx
@@ -0,0 +1,18 @@
+function TaskSearch({ searchTitle, setSearchTitle }) {
+  function handleSearch(event) {
+    setSearchTitle(event.target.value);
+  }
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={searchTitle}
+        onChange={handleSearch}
+        placeholder="Що хочете знайти?"
+      />
+    </div>
+  );
+}
+
+export default TaskSearch;
